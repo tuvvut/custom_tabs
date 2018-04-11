8b98bb1683c42f733cc79163cb54ddd11dd05079
wu <tuvvut@gmail.com>
2018-04-12 02:39:12 +0800

    no message

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-有兩個問題不知道該怎麼解
-
-第 71 行的 CustomTabsCallback 完全不會被執行
-https://github.com/tuvvut/custom_tabs/blob/430c6442bd6aa880ae487f217ad3a02755ccca21/app/src/main/java/com/test/customtabs/MainActivity.java#L71
-
-第 42 行 requestPostMessageChannel 一直都回傳 false
-https://github.com/tuvvut/custom_tabs/blob/430c6442bd6aa880ae487f217ad3a02755ccca21/app/src/main/java/com/test/customtabs/MainActivity.java#L42
-
-理想的執行結果會是，按下按鈕，透過 custom tabs 打開網頁，等了 3 秒後，頁面上會出現 123123 的字串
+理想的執行結果會是，按下按鈕，透過 custom tabs 打開網頁，等了 2 秒後，頁面上會出現 123123 的字串，但始終無法成功顯示
--- app/src/main/java/com/test/customtabs/MainActivity.java
+++ app/src/main/java/com/test/customtabs/MainActivity.java
@@ -1,6 +1,8 @@
 package com.test.customtabs;
 
+import android.app.Service;
 import android.content.ComponentName;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.customtabs.CustomTabsCallback;
@@ -8,13 +10,15 @@ import android.support.customtabs.CustomTabsClient;
 import android.support.customtabs.CustomTabsIntent;
 import android.support.customtabs.CustomTabsServiceConnection;
 import android.support.customtabs.CustomTabsSession;
+import android.support.customtabs.CustomTabsSessionToken;
+import android.support.customtabs.PostMessageServiceConnection;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "CustomTabsClientExample";
     String chromePackageName = "com.android.chrome";
     private EditText url;
@@ -22,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private CustomTabsClient mCustomTabsClient;
     private CustomTabsSession session;
     private String urlString;
+    private PostMessageServiceConnection postMessageServiceConnection;
+    private CustomTabsIntent customTabsIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +37,7 @@ public class MainActivity extends AppCompatActivity {
         open = (Button) findViewById(R.id.open);
 
         urlString = url.getText().toString();
-        open.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                urlString = url.getText().toString();
-                openCustomTabs();
-                open.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        boolean b = session.requestPostMessageChannel(Uri.parse("http://tuvvut.com/"));
-                        int i = session.postMessage("123123", null);
-                        Log.d(TAG, "run " + b + " , " + i);
-                    }
-                }, 3000);
-            }
-        });
-
+        open.setOnClickListener(this);
         CustomTabsClient.bindCustomTabsService(this, chromePackageName, getCustomTabsServiceConnection());
     }
 
@@ -57,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 mCustomTabsClient = client;
                 mCustomTabsClient.warmup(0L);
                 session = mCustomTabsClient.newSession(getCustomTabsCallback());
-                session.mayLaunchUrl(Uri.parse(urlString), null, null);
+                setCustomTabsIntent();
                 Log.d(TAG, "onCustomTabsServiceConnected ");
             }
 
@@ -85,22 +76,66 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onMessageChannelReady(Bundle extras) {
                 super.onMessageChannelReady(extras);
-                Log.d(TAG, "onMessageChannelReady ");
+                boolean i = postMessageServiceConnection.postMessage("123123", null);
+                Log.d(TAG, "onMessageChannelReady " + i);
             }
 
             @Override
             public void onPostMessage(String message, Bundle extras) {
                 super.onPostMessage(message, extras);
-                Log.d(TAG, "onPostMessage ");
+                Log.d(TAG, "onPostMessage " + message);
             }
         };
     }
 
-    private void openCustomTabs() {
-        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        CustomTabsIntent customTabsIntent = builder.build();
+    private void setCustomTabsIntent() {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder(session);
+        customTabsIntent = builder.build();
 
-        customTabsIntent.intent.setPackage(chromePackageName);
+        CustomTabsSessionToken token = CustomTabsSessionToken.getSessionTokenFromIntent(customTabsIntent.intent);
+        postMessageServiceConnection = new PostMessageServiceConnection(token) {
+            @Override
+            public void onBindingDied(ComponentName name) {
+                Log.d(TAG, "onBindingDied");
+            }
+
+            @Override
+            public void onPostMessageServiceDisconnected() {
+                Log.d(TAG, "onPostMessageServiceDisconnected");
+                super.onPostMessageServiceDisconnected();
+            }
+
+            @Override
+            public void onPostMessageServiceConnected() {
+                Log.d(TAG, "onPostMessageServiceConnected");
+
+                boolean b = session.requestPostMessageChannel(Uri.parse("http://tuvvut.com"));
+                Log.d(TAG, "requestPostMessageChannel " + b);
+
+                super.onPostMessageServiceConnected();
+            }
+
+            @Override
+            public boolean bindSessionToPostMessageService(Context context, String packageName) {
+                Log.d(TAG, "bindSessionToPostMessageService");
+                return super.bindSessionToPostMessageService(context, packageName);
+            }
+        };
+
+        postMessageServiceConnection.bindSessionToPostMessageService(this, "com.test.customtabs");
+        bindService(customTabsIntent.intent, postMessageServiceConnection, Service.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        urlString = url.getText().toString();
         customTabsIntent.launchUrl(this, Uri.parse(urlString));
+
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                postMessageServiceConnection.notifyMessageChannelReady(null);
+            }
+        }, 2000);
     }
 }
